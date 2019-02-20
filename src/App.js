0fe001d8881d0068ee98a298d99cbb25e1fea751
Matilda Sjöblom <matilda.sjo96@gmail.com>
2019-02-20 19:27:29 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -14,17 +14,21 @@ class App extends React.Component {
   }
 
   handleChange(date) {
+    //Sets the user's requested date to compare with
     this.setState({
       stopDate: date
     });
   }
 
   handleChangeRaw = (e) => {
+    //Makes the user unable to input own date formats
     e.preventDefault();
   }
 
   countDaysLeft = function () {
+    //24h * 60min * 60sec * 1000millisec
     const oneDay = 24 * 60 * 60 * 1000;
+    //Time in millisec between the dates divided by one day
     return -Math.round((this.state.startDate.getTime() - this.state.stopDate.getTime()) / oneDay);
   }
 
@@ -38,9 +42,10 @@ class App extends React.Component {
           <h3>Pick a date to see how far away it is from today!</h3>
         </div>
         <div id="content">
+          {/*The date picker that sets users' requested date*/}
           <DatePicker
             selected={this.state.stopDate}
-            onSelect={this.handleChange}
+            onChange={this.handleChange}
             onChangeRaw={this.handleChangeRaw}
             id="datepicker"
           />
